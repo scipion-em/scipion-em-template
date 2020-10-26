2b0dda1e7396d7ce0fa9d5827e003b4f04548598
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2020-10-26 09:30:35 +0100

    * Last changes

--- myplugin/protocols/protocol_empiar_download.py
+++ myplugin/protocols/protocol_empiar_download.py
@@ -43,130 +43,87 @@ class EmpiarDownloader(EMProtocol):
     Download image sets from empiar
     """
     _label = 'empiar downloader'
-    _outputClassName = 'SetOfMovies'
-    registerFiles = []
+    _outputClassName = 'SetOfMovies'  # Defining the output class
+    registerFiles = []                # saves the name of the movies that have been downloaded
+    _stepsCheckSecs = 3               # time in seconds to check the steps
 
     def __init__(self, **args):
         EMProtocol.__init__(self, **args)
-        self.stepsExecutionMode = STEPS_PARALLEL
+        self.stepsExecutionMode = STEPS_PARALLEL  # Defining that the protocol contain parallel steps
 
     # --------------- DEFINE param functions ---------------
     def _defineParams(self, form):
         form.addSection(label='Entry')
+        # EMPIAR dataset ID
         form.addParam('entryID', params.StringParam, default='10200',
                       label='EMPIAR ID',
                       important=True,
                       allowsNull=False,
                       help='EMPIAR entry ID')
+        # Stop criteria
         form.addParam('amountOfImages', params.IntParam,
                       default=5,
                       label='Amount of Images',
                       validators=[Positive],
-                      help='Time that the protocol will be running expressed in seconds')
-        form.addParallelSection(threads=2, mpi=1)
+                      help='Time that the protocol will be running expressed '
+                           'in seconds')
+        # Parallel section defining the number of threads and mpi to use
+        form.addParallelSection(threads=3, mpi=1)
 
     # --------------- INSERT steps functions ----------------
 
     def _insertAllSteps(self):
-        self._insertFunctionStep('readXmlFileStep')
-        self._insertFunctionStep('downloadImagesStep')
-        self._insertFunctionStep('closeSetStep', wait=True)
+        # read the dataset xml file from EMPIAR
+        self.readXmlFile = self._insertFunctionStep('readXmlFileStep')
+        # download the movies and register them in pararell
+        self.downloadImages = self._insertFunctionStep('downloadImagesStep')
+        # close the registered dataset set
+        self.closeSet = self._insertFunctionStep('closeSetStep', wait=True)
     # --------------- STEPS functions -----------------------
-
-    def closeSetStep(self):
-        """
-        Close the set
-        """
-        self.outputMovies.setStreamState(SetOfMovies.STREAM_CLOSED)
-        self.outputMovies.write()
-        self._store()
-
-    def _stepsCheck(self):
-        # Input movie set can be loaded or None when checked for new inputs
-        # If None, we load it
-        depStepsList = []
-        if len(self.registerFiles) < self.amountOfImages.get():
-            for file in os.listdir(self._getExtraPath()):
-                if file not in self.registerFiles:
-                    self.registerFiles.append(file)
-                    lastSteps = self._insertFunctionStep('registerImageStep',
-                                                         file, prerequisites=[1])
-                    depStepsList.append(lastSteps)
-
-                if len(self.registerFiles) >= self.amountOfImages.get():
-                    self._steps[2].setStatus(STATUS_NEW)
-                    self._steps[2].addPrerequisites(*depStepsList)
-
-            self.updateSteps()
-
-    def registerImageStep(self, file):
-        """
-        """
-        newImage = Movie(location=self._getExtraPath(file))
-        newImage.setSamplingRate(self.samplingRate.get())
-        self._addMovieToOutput(newImage)
-
-    def _addMovieToOutput(self, movie):
-        """
-        Returns the output set if not available create an empty one
-        :return:
-        """
-        outputSet = None
-        if hasattr(self, 'outputMovies'):
-            outputSet = self.outputMovies
-            outputSet.append(movie)
-        else:
-            outputSet = SetOfMovies.create(self._getPath())
-            outputSet.setSamplingRate(self.samplingRate.get())
-            outputSet.setStreamState(outputSet.STREAM_OPEN)
-            outputSet.append(movie)
-            self._defineOutputs(outputMovies=outputSet)
-        outputSet.write()
-        self._store()
 
     def readXmlFileStep(self):
         """
-        Download file by file from a specific dataset from EMPIAR repository
+        Read the xml file of a specific dataset from EMPIAR repository
         """
-        xmlFileName = self.entryID.get()
-        empiarXmlUrl = 'https://www.ebi.ac.uk/pdbe/emdb/empiar/api/entry/'
-        empiarXmlUrl += xmlFileName
+        xmlFileID = self.entryID.get()  # dataset ID
+        empiarXmlUrl = 'https://www.ebi.ac.uk/pdbe/emdb/empiar/api/entry/'  # URL of EMPIAR API
+        empiarXmlUrl += xmlFileID
         try:
-            xmlFile = requests.get(empiarXmlUrl, allow_redirects=True)
-            content = (json.loads(xmlFile.content.decode('utf-8')))
-            empiarName = 'EMPIAR-' + xmlFileName
-            self.corresponingAuthor = content[empiarName]['corresponding_author']
-            self.organization = String(self.corresponingAuthor['author']['organization'])
-            self.depositionDate = String(content[empiarName]['deposition_date'])
-            self.title = String(content[empiarName]['title'])
-            self.imageSets = content[empiarName]['imagesets']
-            self.releaseDate = String(content[empiarName]['release_date'])
-            self.datasetSize = String(content[empiarName]['dataset_size'])
-            self.empiarName = String(empiarName)
-            self.samplingRate = Float(self.imageSets[0]['pixel_width'])
-            # self.dataFormat = String(content[empiarName]['data_format'])
-
+            xmlFile = requests.get(empiarXmlUrl, allow_redirects=True)  # getting the xml file
+            content = (json.loads(xmlFile.content.decode('utf-8')))  # extract the xml content
+            empiarName = 'EMPIAR-' + xmlFileID
+            self.empiarName = String(empiarName)  # dataset name
+            self.corresponingAuthor = content[empiarName]['corresponding_author']  # dataset authors
+            self.organization = String(self.corresponingAuthor['author']['organization'])  # authors organization
+            self.depositionDate = String(content[empiarName]['deposition_date'])  # dataset deposition date
+            self.title = String(content[empiarName]['title'])  # dataset title
+            self.imageSets = content[empiarName]['imagesets']  # dataset images information
+            self.releaseDate = String(content[empiarName]['release_date'])  # dataset release date
+            self.datasetSize = String(content[empiarName]['dataset_size'])  # dataset size
+            self.samplingRate = Float(self.imageSets[0]['pixel_width'])  # images sampling rate
+            self.dataFormat = String(self.imageSets[0]['data_format'])  # images format
             self._store(self)
         except Exception as ex:
             self.setFailed(msg="There was an error downloading the EMPIAR raw "
-                               "images: %s!!!" %ex)
+                               "images: %s!!!" % ex)
 
     def downloadImagesStep(self):
         """
         This method connect to EMPIAR repository and download a set of images
         into a specific directory
         """
-        # Connection information
-
         # import time
-        # time.sleep(60)
+        # time.sleep(10)
         # return
+
+        # Connection information
         server = 'ftp.ebi.ac.uk'
         username = 'anonymous'
         password = ''
 
         # Directory information
-        directory = '/empiar/world_availability/' + self.entryID.get() + '/data/Movies'
+        directory = ('/empiar/world_availability/' + self.entryID.get() +
+                     '/data/Movies')
 
         # Establish the connection
         ftp = ftplib.FTP(server)
@@ -176,7 +133,7 @@ class EmpiarDownloader(EMProtocol):
         ftp.cwd(directory)
 
         # Loop through files and download each one individually into a specific
-        # directory
+        # directory until the stop criteria met
         imagesCount = 1
         for filename in ftp.nlst():
             fileAbsPath = os.path.join(self._getTmpPath(), filename)
@@ -190,6 +147,64 @@ class EmpiarDownloader(EMProtocol):
                 if imagesCount > self.amountOfImages.get():
                     break
         ftp.close()
+
+    def closeSetStep(self):
+        """
+        Close the registered set
+        """
+        self.outputMovies.setStreamState(SetOfMovies.STREAM_CLOSED)
+        self.outputMovies.write()
+        self._store()
+
+    def _stepsCheck(self):
+        """ Input movie set can be loaded or None when checked for new inputs
+            If None, we load it.
+            To allow streaming register a movies we need to detect a new
+            movie ready to register into the extra path folder
+            Add as prerequisites(registerImageStep) to the last step(closeSetStep)
+        """
+        depStepsList = []
+        if len(self.registerFiles) < self.amountOfImages.get():
+            for file in os.listdir(self._getExtraPath()):
+                if file not in self.registerFiles:
+                    self.registerFiles.append(file)
+                    # Creating a new step to register the new movie
+                    newStep = self._insertFunctionStep('registerImageStep',
+                                                        file,
+                                                        prerequisites=[])
+                    depStepsList.append(newStep)
+                    # adding as prerequisites the new step to closeSetStep
+                    self._steps[self.closeSet - 1].addPrerequisites(*depStepsList)
+
+            if len(self.registerFiles) >= self.amountOfImages.get(): # The closeSetStep is ready to launch
+                self._steps[self.closeSet-1].setStatus(STATUS_NEW)
+
+            # Updating the protocol steps
+            self.updateSteps()
+
+    def registerImageStep(self, file):
+        """
+        Register an image taking into account a file path
+        """
+        newImage = Movie(location=self._getExtraPath(file))
+        newImage.setSamplingRate(self.samplingRate.get())
+        self._addMovieToOutput(newImage)
+
+    def _addMovieToOutput(self, movie):
+        """
+        Returns the output set, if not available create an empty one
+        """
+        if hasattr(self, 'outputMovies'):  # the output is defined
+            outputSet = self.outputMovies
+            outputSet.append(movie)
+        else:
+            outputSet = SetOfMovies.create(self._getPath())
+            outputSet.setSamplingRate(self.samplingRate.get())
+            outputSet.setStreamState(outputSet.STREAM_OPEN)
+            outputSet.append(movie)
+            self._defineOutputs(outputMovies=outputSet)
+        outputSet.write()
+        self._store()
 
     def _summary(self):
         summary = []
